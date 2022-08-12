370e1ae6e518497794e0a41e06359458996b2416
adityasinghskit <adityasinghskit@gmail.com>
2022-08-12 15:46:42 +0530

    added DriverException, updated DriverService, Controller, Dao

--- Cab_booking/src/main/java/com/masai/controller/DriverController.java
+++ Cab_booking/src/main/java/com/masai/controller/DriverController.java
@@ -1,10 +1,16 @@
 package com.masai.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import com.masai.entity.Driver;
@@ -19,5 +25,25 @@ private DriverService dService;
 public ResponseEntity<Driver> insertDriver(@RequestBody Driver driver){
 	Driver addedDriver= dService.insertDriver(driver);
 	return new ResponseEntity<Driver>(addedDriver,HttpStatus.ACCEPTED);
+}
+@GetMapping("/drivers/{id}")
+public ResponseEntity<Driver> viewDriverById(@PathVariable("id") Integer id){
+	Driver foundDriver= dService.viewDriverById(id);
+	return new ResponseEntity<Driver>(foundDriver,HttpStatus.ACCEPTED);
+}
+@PutMapping("/drivers")
+public String updateDriver(@RequestBody Driver driver) {
+	Driver updateddriver=dService.updateDriver(driver);
+	return "Driver updated "+updateddriver;
+}
+@DeleteMapping("/drivers")
+public String deleteDriver(@RequestBody Driver driver) {
+	Driver deletedDriver=dService.deleteDriver(driver);
+	return "Driver deleted "+deletedDriver;
+}
+@GetMapping("/topDrivers")
+public ResponseEntity<List<Driver>> viewBestDrivers(){
+	List<Driver> list=dService.viewBestDriver();
+	return new ResponseEntity<List<Driver>>(list,HttpStatus.ACCEPTED);
 }
 }
--- Cab_booking/src/main/java/com/masai/service/DriverServiceImpl.java
+++ Cab_booking/src/main/java/com/masai/service/DriverServiceImpl.java
@@ -1,9 +1,14 @@
 package com.masai.service;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.masai.entity.Driver;
+import com.masai.exception.DriverException;
+import com.masai.exception.InvalidId;
 import com.masai.repository.DriverDao;
 @Service
 public class DriverServiceImpl implements DriverService {
@@ -15,6 +20,45 @@ public class DriverServiceImpl implements DriverService {
 	public Driver insertDriver(Driver driver) {
 		Driver savedDriver= dDao.save(driver);
 		return savedDriver;
+	}
+
+	@Override
+	public Driver viewDriverById(Integer id) throws InvalidId {
+		if(id<1)
+			throw new InvalidId("Id should be more than 1");
+		
+		Optional<Driver> opt=dDao.findById(id);
+		
+		return opt.orElseThrow(() -> new DriverException("No Driver found for id: "+id));
+	}
+
+	@Override
+	public Driver updateDriver(Driver driver) throws DriverException{
+		Optional<Driver> opt= dDao.findById(driver.getId());
+		if(opt.isPresent())
+			return dDao.save(driver);
+		else
+			throw new DriverException("No Driver found ");
+	}
+
+	@Override
+	public Driver deleteDriver(Driver driver) throws DriverException{
+		Optional<Driver> opt=dDao.findById(driver.getId());
+		if(opt.isPresent()) {
+			dDao.delete(driver);
+			return driver;
+		}
+		else
+			throw new DriverException("No Driver found ");
+	}
+
+	@Override
+	public List<Driver> viewBestDriver() throws DriverException {
+		List<Driver> drivers= dDao.viewBestDriver();
+		if(drivers.size()>0)
+			return drivers;
+		else
+			throw new DriverException("No Driver found with rating>=4.5");
 	}
 
 	
