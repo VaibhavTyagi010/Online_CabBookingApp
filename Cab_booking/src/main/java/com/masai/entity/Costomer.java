861e11506aff5a96e5ca96ee279f3bdd32b8ce18
Amit Kumar <101566236+amit0426@users.noreply.github.com>
2022-08-12 11:40:03 +0530

    Add files via upload
    costomer Entity

---
+++ Cab_booking/src/main/java/com/masai/entity/Costomer.java
@@ -0,0 +1,34 @@
+package com.masai.entity;
+
+import java.sql.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrimaryKeyJoinColumn;
+import javax.validation.constraints.AssertTrue;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class Costomer extends Abstractuser {
+	@PrimaryKeyJoinColumn
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+	private int costomerId;
+	@AssertTrue
+	private boolean journey_status;
+	
+	
+}
