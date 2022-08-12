7e70c05bca47aa6a9d5b457ed61465b94be17a35
Vaibhav Tyagi <93308342+VaibhavTyagi010@users.noreply.github.com>
2022-08-12 13:59:40 +0530

    exceptions

---
+++ Cab_booking/src/main/java/com/masai/exception/MyErrorDetails.java
@@ -0,0 +1,16 @@
+package com.masai.exception;
+
+import java.time.LocalDateTime;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MyErrorDetails {
+	private LocalDateTime timestamp;
+	private String message;
+	private String details;
+
+}
