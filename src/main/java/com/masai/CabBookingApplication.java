e402625ae07baa017cdbc6ceec94539f75b44fb4
dineshahire123 <dinesh_fw15_485@masai.school>
2022-08-12 12:03:06 +0530

    Driver create

---
+++ src/main/java/com/masai/CabBookingApplication.java
@@ -0,0 +1,13 @@
+package com.masai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CabBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CabBookingApplication.class, args);
+	}
+
+}
